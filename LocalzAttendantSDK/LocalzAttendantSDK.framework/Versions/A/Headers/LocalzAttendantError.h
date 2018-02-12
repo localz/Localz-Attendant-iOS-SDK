cd865bb26ae1a6ea5ea2fb02831d6654d156981d
Daniel Sykes-Turner <dst@localz.co>
2018-02-12 17:46:38 +1100

    Added new public files. Rebuilt for iOS 9

---
+++ LocalzAttendantSDK/LocalzAttendantSDK.framework/Versions/A/Headers/LocalzAttendantError.h
@@ -0,0 +1,19 @@
+//
+//  LocalzAttendantError
+//  LocalzAttendantSDK
+//
+//  Created by Melvin Artemas on 30/04/2014.
+//  Copyright (c) 2014 Localz Pty Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <SpotzSDK/SpotzSDK.h>
+
+#define SpotzCNCErrorDomain @"SpotzCNCSDKDomain"
+
+@interface LocalzAttendantError : NSObject
++ (NSError *)errorWithCode:(int)code userInfo:(NSDictionary *)userInfo;
++ (NSError *)errorWithResponse:(NSURLResponse *)response data:(NSDictionary *)data;
++ (NSError *)invalidResponseFromServer;
++ (NSError *)notFound;
+@end
